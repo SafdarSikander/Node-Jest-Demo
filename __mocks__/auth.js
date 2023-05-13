073e6788c0db0d93c56360c39359d178726bd623
Safdar Sikander <safdar_sikander@live.com>
2023-05-13 14:32:23 +0500

    Mocking complete

--- __mocks__/auth.js
+++ __mocks__/auth.js
@@ -1,17 +1,28 @@
-import {  vi } from 'vitest';
+import { vi } from 'vitest';
 
+// TODO: Find the better way to creating mocking
 const mockStmt1 = { get: vi.fn(() => null) };
 const mockStmt2 = { run: vi.fn(() => ({ changes: 1, lastInsertRowid: 1 })) };
 const mockStmt3 = { get: vi.fn(() => ({ id: 1, name: 'John', email: 'test@example.com' })) };
-const registerMocking = () => ({
-    prepare: vi.fn((query) => {
-        if (query.includes('SELECT * FROM users WHERE email')) {
-            return mockStmt1;
-        } else if (query.includes('INSERT INTO users')) {
-            return mockStmt2;
-        } else if (query.includes('SELECT * FROM users WHERE id')) {
-            return mockStmt3;
-        }
-    })
-});
-export { registerMocking };
+
+const registerSuccessMocking = (query) => {
+    if (query.includes('SELECT * FROM users WHERE email')) {
+        return mockStmt1;
+    } else if (query.includes('INSERT INTO users')) {
+        return mockStmt2;
+    } else if (query.includes('SELECT * FROM users WHERE id')) {
+        return mockStmt3;
+    }
+};
+
+const registerFailMocking = () => mockStmt3
+
+const loginSuccessMocking = () => mockStmt3;
+
+const loginFailMocking = () => mockStmt1
+
+const getUserSuccessMocking = () => mockStmt3
+
+const getUserFailMocking = () => mockStmt1
+
+export { registerSuccessMocking, registerFailMocking, loginSuccessMocking, loginFailMocking, getUserSuccessMocking, getUserFailMocking };
